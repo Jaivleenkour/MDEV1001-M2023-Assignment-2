334c4340582a23ae1736276d39fd051ffda9915d
Jaivleen Kour <jaivleenkour@Jaivleens-MacBook-Pro.local>
2023-06-18 19:41:08 -0400

    updated the DataViewController file

--- MDEV1001-M2023-Assignment-2/DataViewController.swift
+++ MDEV1001-M2023-Assignment-2/DataViewController.swift
@@ -70,73 +70,13 @@ class DataViewController: UIViewController , UITableViewDelegate, UITableViewDat
                    }
             let imgData = movie.thumbnail != nil ? UIImage(data: movie.thumbnail!) : nil
             cell.thumbnailImage?.image = imgData ?? selectedPosterImage
-
-            
-         /*   // Set the poster image
-            let imageName = "Image\(indexPath.row + 1)"
-            if let image = UIImage(named: imageName) {
-                cell.thumbnailImage.image = image
-            }*/
-     /*
-       //     print("type", movie.thumbnail ?? "nil")
-            var type = movie.thumbnail ?? "no"
-            if  type == "no" || type ==  "" {
-                cell.thumbnailImage.image = UIImage(named: "appleicon")}
-            else{
-                    cell.thumbnailImage.image = UIImage(url: URL(string: type))
-                }
-            */
-           
-           // cell.thumbnailImage.image = UIImage(url: URL(string: movie.thumbnail!))
-           
-          // Create URL
-          //  let url = URL(string: "\(String(describing: movie.thumbnail))")
-           // print("img",movie.thumbnail)
-           // print("url",url)
-                // Fetch Image Data
-//                if let data = try? Data(contentsOf: url) {
-//                    // Create Image and Update Image View
-//                    cell.thumbnailImage.image = UIImage(data: data)
-//                }
-            /*
-            let imageURL = URL(string: type )
-
-            // Creating a session object with the default configuration.
-            // You can read more about it here https://developer.apple.com/reference/foundation/urlsessionconfiguration
-            let session = URLSession(configuration: .default)
-            // Define a download task. The download task will download the contents of the URL as a Data object and then you can do what you wish with that data.
-            let downloadPicTask = session.dataTask(with: movie.thumbnail) { (data, response, error) in
-                // The download has finished.
-                if let e = error {
-                    print("Error downloading cat picture: \(e)")
-                } else {
-                    // No errors found.
-                    // It would be weird if we didn't have a response, so check for that too.
-                    if let res = response as? HTTPURLResponse {
-                        print("Downloaded cat picture with response code \(res.statusCode)")
-                        if let imageData = data {
-                            // Finally convert that Data into an image and do what you wish with it.
-                            let image = UIImage(data: imageData)
-                            // Do something with your image.
-                            cell.thumbnailImage.image = image
-                        } else {
-                            print("Couldn't get image: Image is nil")
-                        }
-                    } else {
-                        print("Couldn't get response code for some reason")
-                    }
-                }
-            }
-            downloadPicTask.resume()
-            */
-            
-                   return cell
+            return cell
             
            
         }
     
     
-    // New for ICE5
+    // Tap on particular cell
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
         performSegue(withIdentifier: "AddEditSegue", sender: indexPath)
@@ -219,18 +159,6 @@ class DataViewController: UIViewController , UITableViewDelegate, UITableViewDat
     }
 
 
-}
-extension UIImage {
-  convenience init?(url: URL?) {
-    guard let url = url else { return nil }
-            
-    do {
-      self.init(data: try Data(contentsOf: url))
-    } catch {
-      print("Cannot load image from url: \(url) with error: \(error)")
-      return nil
-    }
-  }
 }
 
 
